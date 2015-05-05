cd48943c692a35b388c27f4a4f18769719a63f45
John <jdonnal@mit.edu>
2015-05-05 12:56:15 -0400

    added post script to simulate plug update

--- inc/server.h
+++ inc/server.h
@@ -4,15 +4,35 @@
 #define SERVER_BUF_SIZE 300
 
 typedef struct power_pkt_struct {
-  uint8_t valid;          //struct valid flag
-  int32_t vrms;           //RMS voltage
-  int32_t irms;           //RMS current
-  int32_t watts;          //watts
-  int32_t pavg;           //Average power (30s window)
-  int32_t freq;           //Line frequency
-  int32_t pf;             //Power factor
-  int32_t kwh;            //kWh since turn on
+  uint8_t status;             //struct valid flag
+  char timestamp[40];         //YYMMDD HH:MM:ss
+  int32_t vrms[10];           //RMS voltage
+  int32_t irms[10];           //RMS current
+  int32_t watts[10];          //watts
+  int32_t pavg[10];           //Average power (30s window)
+  int32_t freq[10];           //Line frequency
+  int32_t pf[10];             //Power factor
+  int32_t kwh[10];            //kWh since turn on
 } power_pkt;
+
+
+typedef struct power_sample_struct {
+  uint8_t valid;             //struct valid flag
+  char timestamp[40];         //YYMMDD HH:MM:ss
+  int32_t vrms[10];           //RMS voltage
+  int32_t irms[10];           //RMS current
+  int32_t watts[10];          //watts
+  int32_t pavg[10];           //Average power (30s window)
+  int32_t freq[10];           //Line frequency
+  int32_t pf[10];             //Power factor
+  int32_t kwh[10];            //kWh since turn on
+} power_sample;
+
+#define POWER_PKT_EMPTY      0  //all fields are empty
+#define POWER_PKT_FILLING    1  //fields are being filled
+#define POWER_PKT_READY      2  //all fields full, ready to send
+#define POWER_PKT_TX_IN_PROG 3  //transmission in progress
+#define POWER_PKT_TX_FAIL    4  //failed transmission
 
 
 extern power_pkt wemo_power;
