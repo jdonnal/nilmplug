52ddb2b6a18da7e9a760f07f6384e3e03cc0a606
John Donnal <jdonnal@gmail.com>
2022-02-03 20:34:53 -0500

    added event support

--- software/nilmplug/modules/reader.py
+++ software/nilmplug/modules/reader.py
@@ -32,19 +32,51 @@ class PlugReader(ReaderModule):  # pragma: no cover
             self.event_stream = await self.node.event_stream_get(parsed_args.event_stream)
 
     def custom_args(self, parser):
+        parser.add_argument("--load-name", help="name of load", default="load")
         parser.add_argument("--plug-serial", help="plug serial number")
         parser.add_argument("--plug-ip", help="plug ip address")
         parser.add_argument("--event-stream", help="Joule event stream for on/off events")
 
     async def run(self, parsed_args, output):
+        load_name = parsed_args.load_name
         last_ts = joule.utilities.time_now()
+        event = None
+        avg_power_acc = 0
+        avg_power_count = 0
+        start_kwh = None
         await self.plug.set_rtc()
         while not self.stop_requested:
             data = await self.plug.get_data(last_ts)
             if data is not None:
+                for row in data:
+                    ts = row[0]
+                    avg_power = row[4]
+                    kwh = row[7]
+                    if avg_power > 5:  # load is on
+                        if event is None:  # load just turned on
+                            event = joule.api.Event(ts, end_time=ts)
+                            start_kwh = kwh
+                        avg_power_count += 1
+                        avg_power_acc += avg_power
+                    if avg_power < 5 and event is not None:  # load turned off
+                        total_kwh = kwh - start_kwh
+                        avg_power = avg_power_acc / avg_power_count
+                        event.content = {
+                            'average_power': avg_power,
+                            'kwh': total_kwh,
+                            'name': load_name
+                        }
+                        event.end_time = ts
+                        await self.node.event_stream_write(self.event_stream, [event])
+                        event = None
+                        avg_power_acc = 0
+                        avg_power_count = 0
+                        start_kwh = None
+
                 await output.write(data)
                 last_ts = data[-1][0]
             await asyncio.sleep(20)
+
 
 def main():  # pragma: no cover
     r = PlugReader()
--- software/setup.py
+++ software/setup.py
@@ -5,21 +5,22 @@ from setuptools import setup, find_packages
 # Run setup
 setup(name='nilmplug',
       description='Smart plug to support NILM systems',
-      version = '0.6.0',
-      url = 'http://git.wattsworth.net/nilm/wemo-firmware.git',
-      author = 'John Donnal',
-      author_email = 'donnal@usna.edu',
-      long_description = "Smart plug based on Belkin WeMo",
-      license = "Proprietary",
-      install_requires = [ 'requests >= 2.0'
-                           ],
-      packages = [ 'nilmplug'],
+      version='0.6.0',
+      url='http://git.wattsworth.net/nilm/wemo-firmware.git',
+      author='John Donnal',
+      author_email='donnal@usna.edu',
+      long_description="Smart plug based on Belkin WeMo",
+      license="Proprietary",
+      install_requires=['requests >= 2.0'
+                        ],
+      packages=['nilmplug'],
       include_package_data=True,
-      entry_points = {
+      entry_points={
           'console_scripts': [
               'nilm-plug = nilmplug.plug_wrapper:main',
-              'jim-term = nilmplug.terminal:main',
-              ],
-          },
-      zip_safe = False,
+              'nilm-plug-reader = nilmplug.modules.reader:main',
+              'jim-term = nilmplug.scripts.terminal:main'
+          ],
+      },
+      zip_safe=False,
       )
